dd36de9f2815e3f89e8601a6bdffe9d531463943
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-06-14 13:29:02 +0100

    done

--- src/server.js
+++ src/server.js
@@ -1,26 +1,14 @@
-// src/server.js
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import env from './utils/env.js';
-// ... Решта імпортів
 import { getAllStudents, getStudentById } from './services/students.js';
 const PORT = Number(env('PORT', '3000'));
-// // const PORT = 3000;
-// app.get('/', (req, res) => {
-//   res.json({ message: 'Hello world!' });
-// });
-// app.get((req, res) => {
-//   res.status(404).json({ message: 'Not Found' });
-// });
-//
-// });
+
 const startServer = () => {
-  // ... Решта коду функції
   const logger = pino({ transport: { target: 'pino-pretty' } });
 
   const app = express();
-  //   app.use(express.json());
   app.use(logger);
   app.use(cors());
 
@@ -34,7 +22,8 @@ const startServer = () => {
 
   app.get('/students/:studentId', async (req, res, next) => {
     const { studentId } = req.params;
-    const student = await getStudentById(studentId); // Відповідь, якщо контакт не знайдено
+      const student = await getStudentById(studentId);
+      // Відповідь, якщо контакт не знайдено
     if (!student) {
       res.status(404).json({
         message: 'Student not found',
@@ -51,6 +40,5 @@ const startServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-  // ... Решта коду функції
 };
 export default startServer;
