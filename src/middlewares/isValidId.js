0aa9aedce4fbe1d93600f878665741427b932fdc
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-06-25 19:38:36 +0100

    Update isValidId.js

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -4,7 +4,7 @@ import createHttpError from 'http-errors';
 export const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    next(createHttpError(404, 'Id not found'));
+    return next(createHttpError(404, 'Id not found'));
   }
   next();
 };
