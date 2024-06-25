06bee49a5a5d8cee1d17b4664e14736161af1fee
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-06-25 19:13:08 +0100

    page

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -6,9 +6,11 @@ import {
   updateStudent,
 } from '../services/students.js';
 import createHttpError from 'http-errors';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getStudentsController = async (req, res) => {
-  const students = await getAllStudents();
+  const { page, perPage } = parsePaginationParams(req.query);
+  const students = await getAllStudents({ page, perPage });
   res.json({
     status: 200,
     message: 'Successfully found students!',
--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,12 +1,10 @@
 import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
-const isValidId = (req, res, next) => {
+export const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
     next(createHttpError(404, 'Id not found'));
   }
-
   next();
 };
-export default isValidId;
