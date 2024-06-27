1917f43fb7ead40894e6037db3a38cbfb85f0d61
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-06-27 20:11:59 +0100

    Update isValidId.js

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,9 +2,9 @@ import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
 const isValidId = (req, res, next) => {
-  const { id } = req.params;
-  if (!isValidObjectId(id)) {
-    next(createHttpError(404, `${id} not valid id`));
+  const { studentId } = req.params;
+  if (!isValidObjectId(studentId)) {
+    next(createHttpError(404, `${studentId} not valid id`));
   }
   next();
 };
